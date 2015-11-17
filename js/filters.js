3ccc2deea5d99c3b6de4cb7d25767ed43bf132b3
Martin Leopold <m@martinleopold.com>
2015-11-17 19:52:19 +0100

    added filter for relative calendar time

--- js/filters.js
+++ js/filters.js
@@ -1,9 +1,25 @@
-var f = angular.module('dc.filters', []);
+var f = angular.module('dc.filters', []); // create filters module
 
-/* Compute the begin time of a dinner. */
+
+/**
+ * Compute begin time of a dinner.
+ * @param  {dinner} 'dinner'
+ */
 f.filter('getBeginTime', function() {
    return function(dinner) {
-    return dinner.dineinAt || dinner.takeawayFrom;
+      return dinner.dineinAt || dinner.takeawayFrom;
+   };
+});
 
+
+/**
+ * Calendar Time relative to now.
+ * Customize like so: http://momentjs.com/docs/#/customization/calendar/
+ * @param  {string} 'timeStr' moment.js-parsable time string
+ * @return {string} e.g. 'Today', 'Tomorrow at 2:30', 'Yesterday', etc.)
+ */
+f.filter('calendarTime', function() {
+   return function(timeStr) {
+      return moment(timeStr).calendar();
    };
 });
