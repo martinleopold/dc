d1c8acca4bbc2677fe1fb64c14d3658f1d8804f7
Martin Leopold <m@martinleopold.com>
2015-11-25 14:14:00 +0100

    updated maps test

--- js/services/maps.js
+++ js/services/maps.js
@@ -1,24 +1,44 @@
 /* global google */
-angular.module('dc.maps', []) // define module (with dependencies)
+angular.module('dc.maps', ['uiGmapgoogle-maps']) // define module (with dependencies)
 
-.factory('maps', ['$q', function($q) {
-    return {
+.factory('maps', ['$q', 'uiGmapGoogleMapApi', function($q, uiGmapGoogleMapApi) {
+   function compileAddress(address) {
+      // 'Blutgasse 1, 1010 Wien, Austria'
+      if (!address) return '';
+      if (typeof address == 'string') return address;
+      var str = '';
+      str += address.street || '';
+      str += str && address.no ? ' ' + address.no : (address.no || '');
+      str += str && address.zip ? ', ' + address.zip : (address.zip || '');
+      str += str && address.city ? ' ' + address.city : (address.city || '');
+      str += str && address.country ? ', ' + address.country : (address.country || '');
+      return str;
+   }
+
+   return {
+      //
+      compileAddress,
 
       //
       addressToLocation(address) {
-         return $q(function resolver(resolve, reject) {
-            var geocoder = new google.maps.Geocoder();
-            var request = { 'address': address } ;
-            geocoder.geocode(request, function callback(results, status) {
-               if (status != 'OK') {
-                  reject(status);
-                  return;
-               }
-               var result = results[0];
-               resolve({
-                  location : [result.geometry.location.lat(), result.geometry.location.lng()],
-                  formatted_address : result.formatted_address,
-                  partial_match : result.partial_match
+         return uiGmapGoogleMapApi.then(function (gmaps) {
+            return $q(function resolver(resolve, reject) {
+               var geocoder = new gmaps.Geocoder();
+               var request = { 'address': compileAddress(address) } ;
+               geocoder.geocode(request, function callback(results, status) {
+                  if (status != 'OK') {
+                     reject(status);
+                     return;
+                  }
+                  var result = results[0];
+                  resolve({
+                     location : {
+                        latitude: result.geometry.location.lat(),
+                        longitude: result.geometry.location.lng()
+                     },
+                     formatted_address : result.formatted_address,
+                     partial_match : result.partial_match
+                  });
                });
             });
          });
