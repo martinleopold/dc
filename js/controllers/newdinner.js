c203fc93789cfb3ace4dc281e0c241474ceabf28
Martin Leopold <m@martinleopold.com>
2015-11-26 18:35:29 +0100

    add current user address and location to dinner

--- js/controllers/newdinner.js
+++ js/controllers/newdinner.js
@@ -7,17 +7,21 @@ angular.module('dc.controllers')
    console.log('Controller: newdinner');
    resumeSession($scope);
 
-   $scope.dinner = {
-      isPublic: true
-   };
+   $scope.dinner = {};
 
-   if ($scope.user) {
-      $scope.dinner.hostedByUser = $scope.user.userId;
-   }
-
+   /*
+    * Create dinner
+    */
    $scope.create = function() {
       var dinner = _.cloneDeep($scope.dinner);
 
+      if ($scope.user) {
+         dinner.hostedByUser = $scope.user.userId;
+         dinner.address = $scope.user.address;
+         dinner.location = $scope.user.location;
+      }
+
+      dinner.isPublic = true;
       dinner.dineinAt = moment(dinner.dineinAt).toISOString();
       dinner.takeawayFrom = moment(dinner.takeawayFrom).toISOString();
       dinner.takeawayUntil = moment(dinner.takeawayUntil).toISOString();
